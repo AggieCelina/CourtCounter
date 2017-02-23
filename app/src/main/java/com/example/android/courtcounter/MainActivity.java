edb19994be37a7d99f45992db97298622b8876a2
AggieCelina <agnieszka.c.markiewicz@gmail.com>
2017-02-23 18:12:51 +0100

    remove .idea folder

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.android.courtcounter;
 
+import android.media.AudioManager;
+import android.media.ToneGenerator;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.TextView;
 
@@ -12,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     int timeMove = 0;
     int timeRest = 0;
     int timesRepeat = 0;
+    int nbOfSets = 0;
 
 
     @Override
@@ -90,6 +94,25 @@ add and substract time for move
         Chronometer chrono = (Chronometer) findViewById(R.id.chrono);
         chrono.setBase(SystemClock.elapsedRealtime());
         chrono.stop();
+        chrono.setTextSize(120);
+
+        PlayBeep();
+
+        //enable buttons
+        Button startButton = (Button) findViewById(R.id.start_button);
+        startButton.setEnabled(true);
+        Button movePlusButton = (Button) findViewById(R.id.move_plus_button);
+        movePlusButton.setEnabled(true);
+        Button moveSubButton = (Button) findViewById(R.id.move_sub_button);
+        moveSubButton.setEnabled(true);
+        Button restPlusButton = (Button) findViewById(R.id.rest_plus_button);
+        restPlusButton.setEnabled(true);
+        Button restSubButton = (Button) findViewById(R.id.rest_sub_button);
+        restSubButton.setEnabled(true);
+        Button setPlusButton = (Button) findViewById(R.id.set_plus_button);
+        setPlusButton.setEnabled(true);
+        Button setSubButton = (Button) findViewById(R.id.set_sub_button);
+        setSubButton.setEnabled(true);
 
 
     }
@@ -119,18 +142,47 @@ add and substract time for move
 
     //starts chronometer
 
-    public void startTimer(View view) {
+    public void startTimerMove(View view) {
+
+
+        Chronometer chrono = (Chronometer) findViewById(R.id.chrono);
+        chrono.setTextSize(120);
+        chrono.setBase(SystemClock.elapsedRealtime());
+        chrono.start();
+        countTimerMove(chrono, timeMove + 1);
+        PlayBeep();
+//disable buttons
+        Button startButton = (Button) findViewById(R.id.start_button);
+        startButton.setEnabled(false);
+        Button movePlusButton = (Button) findViewById(R.id.move_plus_button);
+        movePlusButton.setEnabled(false);
+        Button moveSubButton = (Button) findViewById(R.id.move_sub_button);
+        moveSubButton.setEnabled(false);
+        Button restPlusButton = (Button) findViewById(R.id.rest_plus_button);
+        restPlusButton.setEnabled(false);
+        Button restSubButton = (Button) findViewById(R.id.rest_sub_button);
+        restSubButton.setEnabled(false);
+        Button setPlusButton = (Button) findViewById(R.id.set_plus_button);
+        setPlusButton.setEnabled(false);
+        Button setSubButton = (Button) findViewById(R.id.set_sub_button);
+        setSubButton.setEnabled(false);
+
+
+    }
+
+    public void startTimerRest(View view) {
 
         Chronometer chrono = (Chronometer) findViewById(R.id.chrono);
         chrono.setBase(SystemClock.elapsedRealtime());
         chrono.start();
-        countTimer(chrono);
+        countTimerRest(chrono, timeRest + 1);
+        PlayBeep();
 
 
     }
 
     //count ticks and break when ticks reach the value set by user in timeMove
-    public void countTimer(Chronometer chronometer) {
+    public void countTimerMove(Chronometer chronometer, final int timeType) {
 
 
         chronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
@@ -141,15 +193,17 @@ add and substract time for move
             public void onChronometerTick(Chronometer chronometer) {
 
 
-                String timeMoveString = String.format("%02d:%02d", timeMove / 60, timeMove % 60);
+                String timeTypeString = String.format("%02d:%02d", timeType / 60, timeType % 60);
                 //The %02d means: "Format as a decimal number using at least 2 digits, and pad with 0 if less than 2 digits"
                 //Note, that using the modulo 60 pTime % 60 gets the remainder of seconds when dividing by 60.
                 // This is short hand for pTime - (ptime / 60) * 60
                 String currentTime = chronometer.getText().toString();
-                if (currentTime.equals(timeMoveString)) //put time according to you
+                if (currentTime.equals(timeTypeString)) //put time according to you
                 {
                     chronometer.stop();
                     chronometer.setText("00:00");
+                    startTimerRest(chronometer);
+                    PlayBeep();
 
 
                 }
@@ -157,6 +211,55 @@ add and substract time for move
             }
         });
 
+
+    }
+
+
+    public void countTimerRest(Chronometer chronometer, final int timeType) {
+
+
+        chronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+//the OnChronometerTickListener method gets called every time the chronometer has incremented.
+
+
+            @Override
+            public void onChronometerTick(Chronometer chronometer) {
+
+
+                String timeTypeString = String.format("%02d:%02d", timeType / 60, timeType % 60);
+                //The %02d means: "Format as a decimal number using at least 2 digits, and pad with 0 if less than 2 digits"
+                //Note, that using the modulo 60 pTime % 60 gets the remainder of seconds when dividing by 60.
+                // This is short hand for pTime - (ptime / 60) * 60
+                String currentTime = chronometer.getText().toString();
+                if (currentTime.equals(timeTypeString)) //put time according to you
+                {
+                    chronometer.stop();
+                    chronometer.setText("00:00");
+                    nbOfSets = nbOfSets + 1;
+                    if (nbOfSets < timesRepeat) {
+                        startTimerMove(chronometer);
+                    } else {
+                        chronometer.setText("well done!");
+                        chronometer.setTextSize(50);
+                        PlayBeep();
+                    }
+
+
+                }
+
+            }
+        });
+
+
+    }
+
+    /*
+
+    make a beep sound
+     */
+    public void PlayBeep() {
+        ToneGenerator toneG = new ToneGenerator(AudioManager.STREAM_ALARM, 100);
+        toneG.startTone(ToneGenerator.TONE_CDMA_ALERT_CALL_GUARD, 200);
 
     }
 
